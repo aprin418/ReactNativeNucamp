f54614797af29cf87fcd360a538b1ebd4fdd10dd
aprin418 <aprin418@gmail.com>
2021-04-24 09:06:16 -0400

    Persist Redux Store

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { View, Text, ScrollView } from "react-native";
 import { Card } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
